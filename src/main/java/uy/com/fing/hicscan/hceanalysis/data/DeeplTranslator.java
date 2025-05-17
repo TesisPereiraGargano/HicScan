e1b90e57a15528a80f75b7d2665f301a855dd7f3
ValentinaPereira1 <101679403+ValentinaPereira1@users.noreply.github.com>
2025-05-17 19:43:34 -0300

    integracion deepl version uno
    el idioma y el authKey estan harcodeados

--- src/main/java/uy/com/fing/hicscan/hceanalysis/data/DeeplTranslator.java
+++ src/main/java/uy/com/fing/hicscan/hceanalysis/data/DeeplTranslator.java
@@ -1,4 +1,26 @@
 package uy.com.fing.hicscan.hceanalysis.data;
+import com.deepl.api.TextResult;
+import com.deepl.api.Translator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/deepl")
 public class DeeplTranslator {
+
+    //@Value("${deepl.api.key}") si queremos colocar la API KEY DEEPL en APPLICATION.PROPERTIES
+    private String authKey = "528bdd79-3a4c-45ef-95e6-29175c5bc4aa:fx";
+
+    @GetMapping("/translateText")
+    public ResponseEntity<String> translateText(@RequestParam String text) {
+        try {
+            Translator translator = new Translator(authKey);
+            //se podría dejar como parámetro el language de forma que fuera configurable o algo así
+            TextResult result = translator.translateText(text, null, "en-US");
+            return ResponseEntity.ok(result.getText());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Error: " + e.getMessage());
+        }
+    }
+
 }
