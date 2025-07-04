4dbae160635e1a014e4c07a04b17b811fce3f7c6
mateo_gargano <mateogargano@gmail.com>
2025-07-03 22:51:59 -0300

    Added an endpoint for /

--- src/main/java/uy/com/fing/hicscan/hceanalysis/controller/HceApiController.java
+++ src/main/java/uy/com/fing/hicscan/hceanalysis/controller/HceApiController.java
@@ -1,4 +1,11 @@
 package uy.com.fing.hicscan.hceanalysis.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HceApiController {
+    @GetMapping("/")
+    public String index() {
+        return "Servidor funcionando correctamente.";
+    }
 }
