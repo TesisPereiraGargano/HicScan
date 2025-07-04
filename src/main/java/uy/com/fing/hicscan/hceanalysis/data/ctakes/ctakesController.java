bf01b782d090e2fb0cf74751765270067dd7d5ac
ValentinaPereira1 <101679403+ValentinaPereira1@users.noreply.github.com>
2025-07-03 21:29:59 -0300

    ruta a sh

--- src/main/java/uy/com/fing/hicscan/hceanalysis/data/ctakes/ctakesController.java
+++ src/main/java/uy/com/fing/hicscan/hceanalysis/data/ctakes/ctakesController.java
@@ -54,7 +54,7 @@ public class ctakesController {
 
             List<String> comandoEjecucionCtakes = new ArrayList<>();
             comandoEjecucionCtakes.add("/bin/bash"); // ejecuta bash en Linux
-            comandoEjecucionCtakes.add("./ejecutarCtakes.sh");
+            comandoEjecucionCtakes.add("/root/HicScan/src/main/java/uy/com/fing/hicscan/hceanalysis/data/ctakes/ejecutarCtakes.sh");
 
             comandoEjecucionCtakes.add(pipelinePath);
             comandoEjecucionCtakes.add(inputFile.getAbsolutePath());
