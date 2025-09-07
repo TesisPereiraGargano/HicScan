92eb95892adc559242a30cde633174ada880328a
ValentinaPereira1 <101679403+ValentinaPereira1@users.noreply.github.com>
2025-09-07 11:55:04 -0300

    medication expander as a service

--- src/main/java/uy/com/fing/hicscan/hceanalysis/controller/tests/MedicationExpanderTest.java
+++ src/main/java/uy/com/fing/hicscan/hceanalysis/controller/tests/MedicationExpanderTest.java
@@ -1,29 +1,42 @@
-package uy.com.fing.hicscan.hceanalysis.languageexpansion;
+package uy.com.fing.hicscan.hceanalysis.controller.tests;
 
 import com.google.common.collect.BiMap;
 import lombok.extern.slf4j.Slf4j;
 import org.ahocorasick.trie.Emit;
 import org.ahocorasick.trie.Trie;
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import uy.com.fing.hicscan.hceanalysis.data.ctakes.dto.ApiResponse;
+import uy.com.fing.hicscan.hceanalysis.languageexpansion.AhoCorasick;
+import uy.com.fing.hicscan.hceanalysis.languageexpansion.MedicationDictionary;
 
 import java.text.Normalizer;
 import java.util.*;
 
+/*Este módulo provee un API que va a permitir utilizar el diccionario de medicamentos disponible en la web de AGESIC
+ * para poder obtener aquellos medicamentos que se le recetaron al paciente con su nombre comercial
+ * y no escribiendo o describiendo la droga del mismo.*/
 @Slf4j
-@Service
-public class MedicationExpander implements LanguageExpansion {
+@RestController
+@RequestMapping("/medExpander")
+public class MedicationExpanderTest {
     private final AhoCorasick ahoCorasick;
     private final MedicationDictionary medicationDictionary;
 
 
-    public MedicationExpander(AhoCorasick ahoCorasick, MedicationDictionary medicationDictionary) {
+    public MedicationExpanderTest(AhoCorasick ahoCorasick, MedicationDictionary medicationDictionary) {
         this.ahoCorasick = ahoCorasick;
         this.medicationDictionary = medicationDictionary;
     }
 
-    @Override
-    public Map<String, String> obtenerPrincipiosActivos(String inputText) {
-        Map<String, String> res = new HashMap<>();
+
+    @PostMapping("/getDrugsFromComercialName")
+    public ResponseEntity<ApiResponse> obtenerPrincipiosActivos(
+            @RequestParam String inputText
+    ){
         try {
             Trie arbolMedicamentos = this.ahoCorasick.getArbolMedicamentos();
             log.info("El arbolMedicamentos es: {}", arbolMedicamentos.toString());
@@ -36,7 +49,7 @@ public class MedicationExpander implements LanguageExpansion {
                     .toLowerCase()
                     .trim(); //borro espacios al incicio o final
 
-            log.info("El texto normalizado es {}", textoNormalizado);
+            log.info("El texto normalizado es {}",textoNormalizado);
 
             // parsea el texto y obtiene coincidencias
             for (Emit emit : arbolMedicamentos.parseText(textoNormalizado)) {
@@ -47,13 +60,13 @@ public class MedicationExpander implements LanguageExpansion {
             //Busco el principio activo
 
             //lo casteo sólo para probar
-
+            Map<String,String> res = new HashMap<>();
 
             BiMap<String, String> listaNombresMedicamentos = medicationDictionary.getListaNombresMedicamentos();
             HashMap<String, String> listaVMPid = medicationDictionary.getListaVMPid();
             HashMap<String, String> listaPrincipiosActivos = medicationDictionary.getListaPrincipiosActivos();
 
-            for (String med : encontrados) {
+            for (String med : encontrados){
                 //AMP_id
                 String amp_id = listaNombresMedicamentos.inverse().get(med);
                 log.info("amp_id {}: {}", med, amp_id);
@@ -63,15 +76,17 @@ public class MedicationExpander implements LanguageExpansion {
                 //Nombre del principio activo
                 String nomPrincActivo = listaPrincipiosActivos.get(vmp_id);
                 log.info("nomPrincActivo {}", nomPrincActivo);
-                res.put(med, nomPrincActivo);
+                res.put(med,nomPrincActivo);
             }
 
-            return res;
+            ApiResponse respuesta = new ApiResponse("success", "Los medicamentos y su traduccion a principios activos es", res);
+            return ResponseEntity.status(200).body(respuesta);
         } catch (Exception e) {
-            log.error("Error en la expansion de medicamentos");
             e.printStackTrace();
-            return res;
+            ApiResponse respuesta = new ApiResponse("error", "Ocurrió un error inesperado: " + e.getMessage(), new HashMap<>());
+            return ResponseEntity.status(500).body(respuesta);
         }
+
 
     }
 }
