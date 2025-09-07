d72abcdb384d2267d0b140ccf212c4d471170b19
ValentinaPereira1 <101679403+ValentinaPereira1@users.noreply.github.com>
2025-09-07 17:27:15 -0300

    caso de uso v1

--- src/main/java/uy/com/fing/hicscan/hceanalysis/controller/HceApiController.java
+++ src/main/java/uy/com/fing/hicscan/hceanalysis/controller/HceApiController.java
@@ -1,11 +1,32 @@
 package uy.com.fing.hicscan.hceanalysis.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import uy.com.fing.hicscan.hceanalysis.dto.ApiResponse;
+import uy.com.fing.hicscan.hceanalysis.usecases.ProcessHceUseCase;
+
+import java.util.Map;
 
 @RestController
+@RequestMapping("/HCE")
 public class HceApiController {
+    private final ProcessHceUseCase processHceUseCase;
+    private HceApiController(ProcessHceUseCase processHceUseCase){
+        this.processHceUseCase = processHceUseCase;
+    }
+
     @GetMapping("/")
     public String index() {
         return "Servidor funcionando correctamente.";
     }
+
+    @GetMapping("/processHCE")
+    public ApiResponse processHCE(
+            @RequestParam String inputText
+    ){
+        Map<String,String> meds = processHceUseCase.processPlainTextHCE(inputText);
+        return new ApiResponse("success", "Datos extraídos correctamente.", meds);
+    }
+
 }
